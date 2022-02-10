40ac4d551268365da0b143815a3130d234bc9583
Luis Felipe Tejada Padilla <lftejadap@uqvirtual.edu.co>
2022-02-10 11:38:14 -0500

    Add bid, user and auction with tests

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:seller){
+    User.new(email: 'a@a', password:'123456')
+  }
+  subject {
+    described_class.new(title: 'Anything',
+                        description: 'Anything',
+                        start_date: DateTime.now,
+                        end_date: DateTime.now + 1.week,
+                        user_id: 1)
+  }
+
+  describe 'Associations' do
+    it { should belong_to(:user).without_validating_presence }
+    it { should have_many(:bids) }
+  end
+
+  it 'is valid with valid attributes' do
+    auction = Auction.new(title: nil)
+    expect(auction).to_not be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a start_date' do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a end_date' do
+    subject.end_date = nil
+    expect(subject).to_not be_valid
+  end
 end
