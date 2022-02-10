9388f5786d22988935644cbee7b83294807282fd
LFTPadilla <62513607+LFTPadilla@users.noreply.github.com>
2022-02-10 11:51:26 -0500

    Add bid, user and auction with tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject {
+    described_class.new(email: 'a@a',
+                        password: 'some_password'
+  )}
+
+  describe "Validations" do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
